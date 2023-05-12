440fd345ae3443ff5f466624bc66b849d71a89d2
Bruno Carvalho <bruno.carvalho@hotmail.de>
2023-05-12 02:34:32 -0300

    feat: main controller file

--- app/lib/Controller.php
+++ app/lib/Controller.php
@@ -2,30 +2,32 @@
 
 namespace app\lib;
 
-class Controller {
-
+class Controller
+{
     public $content;
     public $route;
 
-    public function __construct($route = null) {
+    public function __construct($route = null)
+    {
         $this->route = $route;
-        if(is_callable(array($this, $route['view']))){
+        if (is_callable(array($this, $route['view']))) {
             $this->$route['view']($route['id']);
-        }else{
+        } else {
             $this->listar();
         }
-        
+
         $this->template();
     }
 
-    public function view($name, Array $options = null) {
-
+    public function view($name, array $options = null)
+    {
+        
         ob_start();
-        ob_implicit_flush(false);
+        ob_implicit_flush(true);
         if (isset($options)) {
             foreach ($options as $key => $value) {
-                $$key = $value;
-                extract($$key, EXTR_OVERWRITE);
+                $key = $value;
+                extract($key, EXTR_OVERWRITE);
             }
         }
 
@@ -34,11 +36,17 @@ class Controller {
         $this->content = ob_get_clean();
     }
 
-    public function template($name = null) {
-        if ($name == null)
+    public function template($name = null)
+    {
+        if ($name == null) {
             $name = "index";
+        }
         $request = $_REQUEST;
         include './public/template/' . $name . '.php';
     }
 
+    public function slayer()
+    {
+        echo 'slaaaaaaaaaaaaaaaaaaaaaayer';
+    }
 }
